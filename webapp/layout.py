479a6146c2fc9cc384169f5cd814c37f85edb8e6
alex <q.armand@hotmail.com>
2024-02-15 19:00:03 +0100

    add more graphs to plot

--- webapp/layout.py
+++ webapp/layout.py
@@ -5,14 +5,77 @@ import dash_bootstrap_components as dbc
 layout = html.Div(
     [
         dbc.Row(
-            dbc.Col(
-                html.Div(
-                    dcc.Graph(id='graph'),
-                    style={'width': '150%', 'height': '150%'}
+            [
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph1'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 1}
                 ),
-                width={"size": 6, "order": 1}
-            ),
-        )
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph2'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 2}
+                ),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph3'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 1}
+                ),
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph4'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 2}
+                ),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph5'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 1}
+                ),
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph6'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 2}
+                ),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph7'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 1}
+                ),
+                dbc.Col(
+                    html.Div(
+                        dcc.Graph(id='graph8'),
+                        style={'width': '100%', 'height': '100%'}
+                    ),
+                    width={"size": 6, "order": 2}
+                ),
+            ]
+        ),
     ],
     id='accordion_all',
     style={'marginLeft': '20rem'}
--- webapp/main.py
+++ webapp/main.py
@@ -4,6 +4,7 @@ from dash import html, dcc
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objs as go
 
 from navbar import navbar
 from sidebar import sidebar
@@ -68,7 +69,14 @@ def hide_or_show_sidebar_while_clicking_on_sidebar_button(n_clicks):
 
 
 @app.callback(
-    Output('graph', 'figure'),
+    [Output('graph1', 'figure'),
+     Output('graph2', 'figure'),
+     Output('graph3', 'figure'),
+     Output('graph4', 'figure'),
+     Output('graph5', 'figure'),
+     Output('graph6', 'figure'),
+     Output('graph7', 'figure'),
+     Output('graph8', 'figure')],
     [Input('gender', 'value'),
      Input('senior_citizen', 'value'),
      Input('phone_service', 'value'),
@@ -94,11 +102,38 @@ def update_graph(gender_value, senior_citizen_value, phone_service_value, intern
     if payment_method_value is not None:
         data_copy = data_copy[data_copy['PaymentMethod'] == GetSidebarElementsValue.get_sidebar_value(payment_method_value, "payment_method")]
 
-    figure = px.scatter(data_copy, x='MonthlyCharges', y='TotalCharges', color='Churn',
+    # Scatter plot for Total Charges vs Monthly Charges
+    figure1 = px.scatter(data_copy, x='MonthlyCharges', y='TotalCharges', color='gender',
                      title='Total Charges vs Monthly Charges',
                      labels={'MonthlyCharges': 'Monthly Charges', 'TotalCharges': 'Total Charges'})
 
-    return figure
+    # Pie chart for Churn
+    churn_counts = data_copy['Churn'].value_counts()
+    figure2 = go.Figure(data=[go.Pie(labels=churn_counts.index, values=churn_counts.values)])
+    figure2.update_layout(title='Churn Distribution')
+
+    # Histogram for MultipleLines
+    figure3 = px.histogram(data_copy, x='MultipleLines', title='Multiple Lines Distribution')
+
+    # Bar chart for StreamingTV
+    figure4 = px.bar(data_copy, x='StreamingTV', title='Streaming TV Distribution', color='StreamingTV')
+
+    # Bar chart for StreamingMovies
+    figure5 = px.bar(data_copy, x='StreamingMovies', title='Streaming Movies Distribution', color='StreamingMovies')
+
+    # Pie chart for PaperlessBilling
+    paperless_billing_counts = data_copy['PaperlessBilling'].value_counts()
+    figure6 = px.pie(names=paperless_billing_counts.index, values=paperless_billing_counts.values, title='Paperless Billing Distribution')
+
+    # Pie chart for OnlineSecurity
+    online_security_counts = data_copy['OnlineSecurity'].value_counts()
+    figure7 = px.pie(names=online_security_counts.index, values=online_security_counts.values, title='Online Security Distribution')
+
+    # Pie chart for OnlineBackup
+    online_backup_counts = data_copy['OnlineBackup'].value_counts()
+    figure8 = px.pie(names=online_backup_counts.index, values=online_backup_counts.values, title='Online Backup Distribution')
+
+    return figure1, figure2, figure3, figure4, figure5, figure6, figure7, figure8
 
 # endregion
 
